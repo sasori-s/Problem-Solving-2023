e45b0c5dd7af3de80a137ba6cccb0b1369046e41
Sams <shams.iqbal@g.bracu.ac.bd>
2023-07-10 19:19:40 +0600

    Completed the LCS version of LCS

---
+++ TechieDelight/String/LCS_anuj_eff.py
@@ -0,0 +1,29 @@
+def dp(m, n, A, B):
+    array_2d = [[0 for i in range(n + 1)]for j in range(m + 1)]
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            array_2d[i][j] = -1
+
+    return lcs(array_2d, m, n, A, B)
+
+def lcs(array_2d, m, n, A, B):
+    if m == 0 or n == 0:
+        return 0
+    
+    if array_2d[m][n] != -1:
+        return array_2d[m][n]
+
+    if str1[m-1] == str2[n-1]:
+        array_2d[m][n] = 1 + lcs(array_2d, m-1, n-1, A, B)
+        return array_2d[m][n]
+    
+    else:
+        array_2d[m][n] = max(lcs(array_2d, m, n-1, A, B), lcs(array_2d, m-1, n, A, B))
+        return array_2d[m][n]
+
+if __name__ == "__main__":
+    str1 = "ABCAB"
+    str2 = "AECB"
+
+    print(dp(len(str1), len(str2), str1, str2))
