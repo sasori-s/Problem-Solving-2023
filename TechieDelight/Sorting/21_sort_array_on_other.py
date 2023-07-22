835aa659622f88dce6e1226eb2525290b17b27e1
Sams <shams.iqbal@g.bracu.ac.bd>
2023-07-22 20:28:24 +0600

    Done till problem 21

---
+++ TechieDelight/Sorting/21_sort_array_on_other.py
@@ -0,0 +1,33 @@
+def customSort(first, second):
+    freq = {}
+    
+    for i in first:
+        freq[i] = freq.get(i, 0) + 1
+
+    index = 0
+
+    for i in second:
+        n = freq.setdefault(i, 0)
+        for _ in range(n):
+            first[index] = i
+            index += 1
+
+        freq.pop(i)
+
+    for key in sorted(freq.keys()):
+        count = freq.get(key)
+
+        while count:
+            first[index] = key
+            count -= 1
+            index += 1
+
+        
+
+
+if __name__ == '__main__':
+    first = [5, 8, 9, 3, 5, 7, 1, 3, 4, 9, 3, 5, 1, 8, 4]
+    second = [3, 5, 7, 2]
+
+    customSort(first, second)
+    print("After sorting the list is: ", first)
