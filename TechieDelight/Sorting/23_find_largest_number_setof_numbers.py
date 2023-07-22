9526041c921045fbda38d01628bc802d054383da
Sams <shams.iqbal@g.bracu.ac.bd>
2023-07-22 20:30:33 +0600

    Done till problem 23

---
+++ TechieDelight/Sorting/23_find_largest_number_setof_numbers.py
@@ -0,0 +1,24 @@
+from functools import cmp_to_key
+
+@cmp_to_key
+def custom_compare(a, b):
+    value1 = str(a) + str(b)
+    value2 = str(b) + str(a)
+
+    if value1 < value2:
+        return 1
+
+    elif value1 > value2:
+        return -1
+
+    else:
+        return 0    
+
+def findLargestNumber(numbers):
+    numbers.sort(key=custom_compare)
+    return ''.join(map(str, numbers))
+
+if __name__ == '__main__':
+    numbers = [10, 68, 97, 9, 21, 12]
+    largestNumber = findLargestNumber(numbers)
+    print(f"The largest number is {largestNumber}")
