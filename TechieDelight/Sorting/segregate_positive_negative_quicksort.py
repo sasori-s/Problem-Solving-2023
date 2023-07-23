828b8134248c3e7639253eedfbe3dc94cabd2d99
Sams <shams.iqbal@g.bracu.ac.bd>
2023-07-23 10:17:52 +0600

    Segregate done in quicksort

---
+++ TechieDelight/Sorting/segregate_positive_negative_quicksort.py
@@ -0,0 +1,19 @@
+def swap(A, i ,j):
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+
+def partition(A):
+    pIndex = 0  
+
+    for i in range(len(A)):
+        if A[i] < 0:
+            swap(A, i, pIndex)
+            pIndex = pIndex + 1
+
+
+
+if __name__ == "__main__":
+    A = [9, -3, 5, -2, -8, -6, 1, 3]
+    partition(A)
+    print(A)
