137d218e70ff65a589105b0d5b5cd04d62c70722
Sams <shams.iqbal@g.bracu.ac.bd>
2023-08-16 23:11:59 +0600

    Started the graph

---
+++ TechieDelight/Graphs/1_build_graphs_matrix.py
@@ -0,0 +1,20 @@
+class Graph:
+    def __init__(self, edges, n):
+         self.adjList = [[] for _ in range(n)]
+
+         for (src, dest) in edges:
+              self.adjList[src].append(dest)
+
+def printGraphs(graph):
+     for src in range(len(graph.adjList)):
+        for dest in graph.adjList[src]:
+             print(f"({src} -> {dest})", end=" ")
+        print()
+
+if __name__ == "__main__":
+     edges = [(0, 1), (1, 2), (2, 0), (2, 1), (3, 2), (4, 5), (5, 4)]
+
+     n = 6
+
+     graph = Graph(edges, n)
+     printGraphs(graph)
