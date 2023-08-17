eb2eb3704aa0c6ae808ea5af028cacdf2c1c7c66
Sams <shams.iqbal@g.bracu.ac.bd>
2023-08-17 14:54:52 +0600

    Done till DFS traversal

---
+++ TechieDelight/Graphs/4_dfs.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Graph:
+    def __init__(self, edges, n):
+        self.adjList = [[] for _ in range(n)]
+
+        for (src, dest) in edges:
+            self.adjList[src].append(dest)
+            self.adjList[dest].append(src)
+
+
+def DFS(graph, v, discovered):
+    discovered[v] = True
+    print(v, end=" ")
+
+    for edge in graph.adjList[v]:
+        if not discovered[edge]:
+            DFS(graph, edge, discovered)
+
+if __name__ == "__main__":
+    edges = [
+        (1, 2), (1, 7), (1, 8), (2, 3), (2, 6), (3, 4),
+        (3, 5), (8, 9), (8, 12), (9, 10), (9, 11)
+    ]
+
+    n = 13
+    discovered = [False] * n
+
+    graph = Graph(edges, n)
+
+    for i in range(n):
+        if not discovered[i]:
+            DFS(graph, i, discovered)
