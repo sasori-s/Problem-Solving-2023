b9f9e00a7c10d3399cf60ec6315ea839637aff72
Sams <shams.iqbal@g.bracu.ac.bd>
2023-08-20 01:35:06 +0600

    Did problem of total paths is digraph

---
+++ TechieDelight/Graphs/15_total_path_digraph.py
@@ -0,0 +1,43 @@
+from collections import deque
+class Graph:
+    def __init__(self, edges, n):
+        self.adjList = [[] for _ in range(n)]
+
+        for (src, dest) in edges:
+            self.adjList[src].append(dest)
+
+def findTotalPaths(graph, src, dest, m):
+    q = deque()
+
+    q.append((src, 0))
+
+    count = 0
+
+    while q:
+        vertex, depth = q.popleft() 
+
+        if depth == m and vertex == dest:
+            count += 1
+
+        if depth > m:
+            break
+
+        for u in graph.adjList[vertex]:
+            q.append((u, depth + 1))
+
+    return count
+
+if __name__ == '__main__':
+    edges = [
+        (0, 6), (0, 1), (1, 6), (1, 5), (1, 2), (2, 3), (3, 4),
+        (5, 2), (5, 3), (5, 4), (6, 5), (7, 6), (7, 1)
+    ]
+
+    n = 8
+
+    graph = Graph(edges, n)
+
+    src, dest = 0, 3
+    m = 4
+
+    print(findTotalPaths(graph, src, dest, m))
