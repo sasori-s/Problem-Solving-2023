54a2cd4d1c9774d81139cb716c26f0ac18b7cf1b
Sams <shams.iqbal@g.bracu.ac.bd>
2023-09-07 21:55:00 +0600

    revision

---
+++ Revision/Binary_Tree/problem7.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def printLevel(root, level):
+    if root is None:
+        return False
+
+    if level == 1:
+        print(root.data, end=" ")
+        return True
+
+    left = printLevel(root.left, level - 1)
+    right = printLevel(root.right, level - 1)
+
+    return left or right
+
+
+def  levelOrderTraversal(root):
+    level = 1
+    while printLevel(root, level):
+        level += 1
+
+
+if __name__ == '__main__':
+ 
+    root = Node(15)
+    root.left = Node(10)
+    root.right = Node(20)
+    root.left.left = Node(8)
+    root.left.right = Node(12)
+    root.right.left = Node(16)
+    root.right.right = Node(25)
+ 
+    levelOrderTraversal(root)
